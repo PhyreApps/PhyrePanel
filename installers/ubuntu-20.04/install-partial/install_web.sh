9614dd434adc511869a820d34cbc7f00cdca5ecc
Bozhidar <bobi@microweber.com>
2024-04-24 23:26:00 +0300

    update

--- installers/ubuntu-20.04/install-partial/install_web.sh
+++ installers/ubuntu-20.04/install-partial/install_web.sh
@@ -21,14 +21,14 @@ MYSQL_SCRIPT
 
 
 # Create database
-PANEL_DB_PASSWORD="$(tr -dc a-za-z0-9 </dev/urandom | head -c 32; echo)"
-PANEL_DB_NAME="phyre$(tr -dc a-za-z0-9 </dev/urandom | head -c 13; echo)"
-PANEL_DB_USER="phyre$(tr -dc a-za-z0-9 </dev/urandom | head -c 13; echo)"
+PHYRE_PANEL_DB_PASSWORD="$(tr -dc a-za-z0-9 </dev/urandom | head -c 32; echo)"
+PHYRE_PANEL_DB_NAME="phyre$(tr -dc a-za-z0-9 </dev/urandom | head -c 13; echo)"
+PHYRE_PANEL_DB_USER="phyre$(tr -dc a-za-z0-9 </dev/urandom | head -c 13; echo)"
 
 mysql -uroot -proot <<MYSQL_SCRIPT
-  CREATE DATABASE $PANEL_DB_NAME;
-  CREATE USER '$PANEL_DB_USER'@'localhost' IDENTIFIED BY '$PANEL_DB_PASSWORD';
-  GRANT ALL PRIVILEGES ON $PANEL_DB_NAME.* TO '$PANEL_DB_USER'@'localhost';
+  CREATE DATABASE $PHYRE_PANEL_DB_NAME;
+  CREATE USER '$PHYRE_PANEL_DB_USER'@'localhost' IDENTIFIED BY '$PHYRE_PANEL_DB_PASSWORD';
+  GRANT ALL PRIVILEGES ON $PHYRE_PANEL_DB_NAME.* TO '$PHYRE_PANEL_DB_USER'@'localhost';
   FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
@@ -42,20 +42,20 @@ mysql -uroot -proot <<MYSQL_SCRIPT
 MYSQL_SCRIPT
 
 # Save mysql root password
-echo "$MYSQL_ROOT_PASSWORD" > /root/.phyre_mysql_root_password
+echo "$MYSQL_ROOT_PASSWORD" > /root/.mysql_root_password
 
 # Configure the application
 cp .env.example .env
 
 sed -i "s/^APP_URL=.*/APP_URL=127.0.0.1:8443" .env
 sed -i "s/^APP_NAME=.*/APP_NAME=PHYRE_PANEL/" .env
-sed -i "s/^DB_DATABASE=.*/DB_DATABASE=$PANEL_DB_NAME/" .env
-sed -i "s/^DB_USERNAME=.*/DB_USERNAME=$PANEL_DB_USER/" .env
-sed -i "s/^DB_PASSWORD=.*/DB_PASSWORD=$PANEL_DB_PASSWORD/" .env
+sed -i "s/^DB_DATABASE=.*/DB_DATABASE=$PHYRE_PANEL_DB_NAME/" .env
+sed -i "s/^DB_USERNAME=.*/DB_USERNAME=$PHYRE_PANEL_DB_USER/" .env
+sed -i "s/^DB_PASSWORD=.*/DB_PASSWORD=$PHYRE_PANEL_DB_PASSWORD/" .env
 sed -i "s/^DB_CONNECTION=.*/DB_CONNECTION=mysql/" .env
 
-sed -i "s/^MYSQl_ROOT_USERNAME=.*/MYSQl_ROOT_USERNAME=$MYSQL_ROOT_USERNAME/" .env
-sed -i "s/^MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD/" .env
+sed -i "s/^MYSQl_ROOT_USERNAME=.*/MYSQl_ROOT_USERNAME=$MYSQL_PHYRE_ROOT_USERNAME/" .env
+sed -i "s/^MYSQL_ROOT_PASSWORD=.*/MYSQL_ROOT_PASSWORD=$MYSQL_PHYRE_ROOT_PASSWORD/" .env
 
 phyre-php artisan key:generate
 phyre-php artisan migrate
