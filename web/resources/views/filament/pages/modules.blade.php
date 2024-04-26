de5bf44a98af18d6fe11c3b8a512b00240ba5a0d
Bozhidar <bobi@microweber.com>
2024-04-26 22:16:19 +0300

    Update modules.blade.php

--- web/resources/views/filament/pages/modules.blade.php
+++ web/resources/views/filament/pages/modules.blade.php
@@ -81,6 +81,16 @@
                                         @endif
                                     </div>
                                 </x-filament::dropdown.list.item>
+                                @if ($module['installed'])
+                                    <x-filament::dropdown.list.item wire:click="openUnistallModal('{{$module['name']}}')">
+                                        <div class="flex gap-2 items-center">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6" viewBox="0 0 24 24">
+                                                <path fill="currentColor" d="M7.615 20q-.69 0-1.152-.462T6 18.385V6H5V5h4v-.77h6V5h4v1h-1v12.385q0 .69-.462 1.152T16.385 20zm2.193-3h1V8h-1zm3.384 0h1V8h-1z"></path>
+                                            </svg>
+                                            Unistall
+                                        </div>
+                                    </x-filament::dropdown.list.item>
+                                @endif
                             </x-filament::dropdown.list>
                         </x-filament::dropdown>
                     </div>
