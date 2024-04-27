2c6a461ac5343b9f5cc9b3adedc68769686ebfd3
Bozhidar <bobi@microweber.com>
2024-04-27 21:22:45 +0300

    Update logs.blade.php

--- web/Modules/Docker/resources/views/filament/infolists/docker-container/logs.blade.php
+++ web/Modules/Docker/resources/views/filament/infolists/docker-container/logs.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div class="bg-black/5 dark:bg-white/5 dark:text-green-400 p-4 rounded">
+    <div class="bg-black/5 dark:bg-white/5 dark:text-green-500 p-4 rounded">
     {!! $this->containerLog !!}
     </div>
 </div>
