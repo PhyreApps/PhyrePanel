4d9048c09718a50551d63b664e4dcd32da762727
Bozhidar <bobi@microweber.com>
2024-04-27 13:33:33 +0300

    Update DockerContainerApi.php

--- web/Modules/Docker/DockerContainerApi.php
+++ web/Modules/Docker/DockerContainerApi.php
@@ -73,7 +73,6 @@ class DockerContainerApi
             shell_exec('mkdir -p ' . $dockerContaienrPath);
         }
 
-        dd($dockerComposeFileContent);
         $dockerComposeFile = $dockerContaienrPath . '/docker-compose.yml';
         file_put_contents($dockerComposeFile, $dockerComposeFileContent);
 
