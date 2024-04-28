c7b57e03d3f15d5b4d5c6df3b6668d242fa6557c
Bozhidar <bobicloudvision@gmail.com>
2024-04-28 18:40:29 +0300

    update

--- installers/ubuntu-22.04/install.sh
+++ installers/ubuntu-22.04/install.sh
@@ -67,9 +67,9 @@ DISTRO_VERSION=${DISTRO_VERSION//\"/} # Remove quotes from version string
 DISTRO_NAME=$(cat /etc/os-release | grep -w "NAME" | cut -d "=" -f 2)
 DISTRO_NAME=${DISTRO_NAME//\"/} # Remove quotes from name string
 
-LOG_JSON='{"os": "'$DISTRO_NAME-$DISTRO_VERSION'", "hostname": "'$HOSTNAME'", "ip": "'$IP_ADDRESS'"}'
+LOG_JSON='{"os": "'$DISTRO_NAME-$DISTRO_VERSION'", "host_name": "'$HOSTNAME'", "ip": "'$IP_ADDRESS'"}'
 
-curl -s https://phyrepanel.com/api/phyre-installation-log -X POST -H "Content-Type: application/json" -d $LOG_JSON
+curl -s https://phyrepanel.com/api/phyre-installation-log -X POST -H "Content-Type: application/json" -d "$LOG_JSON"
 #!/bin/bash
 
 wget https://github.com/PhyreApps/PhyrePanelWebCompiledVersions/raw/main/phyre-web-panel.zip
