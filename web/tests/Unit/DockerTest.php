8dc9b2537b3d9cab5d19e28479d85febc11dc5d5
Bozhidar <bobicloudvision@gmail.com>
2024-04-30 00:07:17 +0300

    Update DockerTest.php

--- web/tests/Unit/DockerTest.php
+++ web/tests/Unit/DockerTest.php
@@ -5,6 +5,7 @@ namespace tests\Unit;
 use App\Models\User;
 use Livewire\Livewire;
 use Modules\Docker\Filament\Clusters\Docker\Pages\DockerCatalog;
+use Modules\Docker\Filament\Clusters\Docker\Resources\DockerContainerResource;
 use Modules\Docker\PostInstall;
 use Tests\TestCase;
 
@@ -29,15 +30,35 @@ class DockerTest extends TestCase
 
         $this->actingAs(User::factory()->create());
 
-        $livewireCatalogIndex = Livewire::test(DockerCatalog::class)
-            ->set('keyword', 'nginx')
-            ->assertSee('nginx');
+        $dockerImage = 'nginx';
+
+        $dockerCatalogTest = Livewire::test(DockerCatalog::class);
+        $livewireCatalogIndex = $dockerCatalogTest->set('keyword', $dockerImage)
+            ->assertSee($dockerImage);
 
         $viewData = $livewireCatalogIndex->viewData('dockerImages');
         $this->assertNotEmpty($viewData);
 
         $livewireCatalogIndex->set('keyword', 'non-existing-image')
             ->assertDontSee('non-existing-image');
+
+        $pullDockerImage = $dockerCatalogTest->call('pullDockerImage', $dockerImage)
+            ->assertSee('Pull Docker Image');
+
+        $isDokerImagePulled = false;
+        $pullLog = '';
+        for ($i = 0; $i < 300; $i++) {
+            $pullLog = @file_get_contents($pullDockerImage->get('pullLogFile'));
+            if (strpos($pullLog, 'DONE!') !== false) {
+                $isDokerImagePulled = true;
+                break;
+            }
+            sleep(1);
+        }
+        $this->assertTrue($isDokerImagePulled);
+
+        $this->assertNotEmpty($pullLog);
+        $this->assertStringContainsString('DONE!', $pullLog);
 
     }
 
