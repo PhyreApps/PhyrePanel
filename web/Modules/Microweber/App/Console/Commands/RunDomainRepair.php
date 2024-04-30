1db5629c952e839c3e197c83dd3bccb744079dfb
Bozhidar <bobicloudvision@gmail.com>
2024-04-30 17:08:36 +0300

    Update RunDomainRepair.php

--- web/Modules/Microweber/App/Console/Commands/RunDomainRepair.php
+++ web/Modules/Microweber/App/Console/Commands/RunDomainRepair.php
@@ -42,12 +42,17 @@ class RunDomainRepair extends Command
                     continue;
                 }
 
+                if (is_link($mwInstallation->installation_path.'/userfiles/modules/logo')) {
+                    $this->info('Symlink exists: ' . $mwInstallation->installation_path.'/userfiles/modules/logo');
+                    continue;
+                }
+
                 $this->info('Repair domain: ' . $domain->domain);
 
                 $microweberReinstall = new MicroweberReinstaller();
                 $microweberReinstall->setSymlinkInstallation();
                 $microweberReinstall->setChownUser($domain->domain_username);
-                $microweberReinstall->setPath($domain->domain_public);
+                $microweberReinstall->setPath($mwInstallation->installation_path);
                 $microweberReinstall->setSourcePath(config('microweber.sharedPaths.app'));
 
                 $reinstallStatus = $microweberReinstall->run();
