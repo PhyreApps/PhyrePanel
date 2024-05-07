cc820a767159c2db9c9504e04ca4b92ccc0e65d9
Bozhidar <bobi@microweber.com>
2024-05-07 14:48:20 +0300

    update

--- web/config/app.php
+++ web/config/app.php
@@ -167,7 +167,6 @@ return [
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
-        App\Providers\HorizonServiceProvider::class,
         App\Providers\Filament\AdminPanelProvider::class,
         App\Providers\RouteServiceProvider::class,
 
