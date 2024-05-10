c164b935261f6be3c52146ecf6c6d81ee43496d2
Bozhidar <bobi@microweber.com>
2024-05-10 14:20:22 +0300

    update

--- web/app/Filament/Resources/BackupResource/Pages/ManageBackups.php
+++ web/app/Filament/Resources/BackupResource/Pages/ManageBackups.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Resources\BackupResource\Pages;
 
 use App\Filament\Resources\BackupResource;
+use App\Jobs\RestoreBackup;
 use Filament\Actions;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Form;
 use Filament\Pages\Concerns\ExposesTableToWidgets;
 use Filament\Resources\Components\Tab;
 use Filament\Resources\Pages\ManageRecords;
@@ -17,8 +20,28 @@ class ManageBackups extends ManageRecords
     protected function getActions(): array
     {
         return [
+            Actions\Action::make('restore')
+                ->icon('heroicon-o-cloud-arrow-up')
+                ->slideOver()
+                ->modalHeading('Restore data from backup file')
+                ->form(function () {
+                    return [
+                        FileUpload::make('backupFile')
+                            ->placeholder('Upload your backup file to restore data')
+                            ->helperText('Supported file types: .zip')
+                            ->disk('local')
+                            ->directory('backup-restores')
+                            ->visibility('private')
+                            ->acceptedFileTypes(['application/zip'])
+                    ];
+                })->afterFormValidated(function (array $data) {
+
+                    RestoreBackup::dispatch($data['backupFile']);
+
+                }),
+
             Actions\CreateAction::make()
-                ->size('sm')
+                ->icon('heroicon-o-clock')
                 ->slideOver(),
         ];
     }
---
+++ web/app/Jobs/RestoreBackup.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Storage;
+
+class RestoreBackup implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $backupFile = null;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct($backupFile)
+    {
+        $this->backupFile = $backupFile;
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+
+        echo "Restoring backup file: " . $this->backupFile . "\n";
+
+        $file = Storage::disk('local')->get($this->backupFile);
+        if (!$file) {
+            echo "Backup file not found\n";
+            return;
+        }
+
+        
+
+    }
+}
