081b67f1efd444e13581459a2eb1e0f0faba487e
Bozhidar <bobi@microweber.com>
2024-05-13 19:46:07 +0300

    Update file-manager.blade.php

--- web/resources/views/filament/pages/file-manager.blade.php
+++ web/resources/views/filament/pages/file-manager.blade.php
@@ -1,3 +1,12 @@
-<div>
-{{ $this->table }}
-</div>
+<x-filament-panels::page
+    @class([
+        'fi-resource-manage-files-page',
+        'fi-resource-' . str_replace('/', '-', $this->getResource()::getSlug()),
+    ])
+>
+
+    <div>
+        {{ $this->table }}
+    </div>
+
+</x-filament-panels::page>
