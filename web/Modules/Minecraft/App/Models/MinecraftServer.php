cacaa5c45279ff204dc0c248d18c850df2a674a0
Bozhidar <bobi@microweber.com>
2024-05-13 11:40:34 +0300

    update

--- web/Modules/Minecraft/App/Models/MinecraftServer.php
+++ web/Modules/Minecraft/App/Models/MinecraftServer.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Minecraft\App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Modules\Minecraft\Database\factories\MinecraftServerFactory;
@@ -15,5 +16,38 @@ class MinecraftServer extends Model
      */
     protected $fillable = [];
 
+
+    protected static function booted(): void
+    {
+        static::addGlobalScope('customer', function (Builder $query) {
+            if (auth()->check() && auth()->guard()->name == 'web_customer') {
+                $query->where('customer_id', auth()->user()->id);
+            }
+        });
+    }
+
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+
+
+        });
+
+        static::created(function ($model) {
+
+
+        });
+
+        static::deleting(function ($model) {
+
+
+
+
+        });
+
+    }
 
 }
---
+++ web/Modules/Minecraft/resources/views/actions/minecraft-docker-yaml.blade.php
@@ -0,0 +1,9 @@
+services:
+    minecraft-server:
+        image: itzg/minecraft-server
+        tty: true
+        stdin_open: true
+        ports:
+            - "25565:25565"
+        environment:
+            EULA: "TRUE"
