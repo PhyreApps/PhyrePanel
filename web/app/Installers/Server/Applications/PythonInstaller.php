f0deeb24b67fd3fe9fa423b2ea9ad0e79cf05c98
Bozhidar <bobi@microweber.com>
2024-05-15 01:30:35 +0300

    update

--- web/app/Installers/Server/Applications/PythonInstaller.php
+++ web/app/Installers/Server/Applications/PythonInstaller.php
@@ -21,19 +21,20 @@ class PythonInstaller
     public function install()
     {
         $commands = [];
+        $commands[] = 'export DEBIAN_FRONTEND=noninteractive';
         foreach ($this->pythonVersions as $pythonVersion) {
-            $commands[] = 'apt-get install -y python' . $pythonVersion;
-            $commands[] = 'apt-get install -y python' . $pythonVersion . '-dev';
-            $commands[] = 'apt-get install -y python' . $pythonVersion . '-venv';
-            $commands[] = 'apt-get install -y python' . $pythonVersion . '-setuptools';
-            $commands[] = 'apt-get install -y python' . $pythonVersion . '-wheel';
+            $commands[] = 'apt-get install -yq python' . $pythonVersion;
+            $commands[] = 'apt-get install -yq python' . $pythonVersion . '-dev';
+            $commands[] = 'apt-get install -yq python' . $pythonVersion . '-venv';
+            $commands[] = 'apt-get install -yq python' . $pythonVersion . '-setuptools';
+            $commands[] = 'apt-get install -yq python' . $pythonVersion . '-wheel';
         }
 
         // Install Apache Passenger
         $commands[] = 'curl https://oss-binaries.phusionpassenger.com/auto-software-signing-gpg-key.txt | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/phusion.gpg >/dev/null';
         $commands[] = "sudo sh -c 'echo deb https://oss-binaries.phusionpassenger.com/apt/passenger jammy main > /etc/apt/sources.list.d/passenger.list'";
         $commands[] = 'apt-get update';
-        $commands[] = 'sudo apt-get install -y libapache2-mod-passenger';
+        $commands[] = 'sudo apt-get install -yq libapache2-mod-passenger';
         $commands[] = 'sudo a2enmod passenger';
         $commands[] = 'sudo service apache2 restart';
 
