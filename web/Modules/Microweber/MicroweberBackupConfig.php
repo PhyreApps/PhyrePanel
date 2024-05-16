08087255ebaf943c68ff6345cb98d59188f59f00
Bozhidar <bobi@microweber.com>
2024-05-16 13:01:53 +0300

    Update MicroweberBackupConfig.php

--- web/Modules/Microweber/MicroweberBackupConfig.php
+++ web/Modules/Microweber/MicroweberBackupConfig.php
@@ -8,18 +8,17 @@ class MicroweberBackupConfig extends BackupConfigBase
 {
     public array $excludePaths = [
 
-        '/home/*/public_html/storage/framework/cache',
-        '/home/*/public_html/storage/framework/views',
-        '/home/*/public_html/userfiles/cache',
-        '/home/*/public_html/userfiles/media/thumbnails',
-        '/home/*/public_html/storage/framework/sessions',
+        '/home/*/public_html/storage/framework/cache/*',
+        '/home/*/public_html/storage/framework/views/*',
+        '/home/*/public_html/userfiles/cache/*',
+        '/home/*/public_html/userfiles/media/thumbnails/*',
+        '/home/*/public_html/storage/framework/sessions/*',
 
-        '/home/*/domains/*/public_html',
-        '/home/*/domains/*/public_html/storage/framework/cache',
-        '/home/*/domains/*/public_html/storage/framework/views',
-        '/home/*/domains/*/public_html/userfiles/cache',
-        '/home/*/domains/*/public_html/userfiles/media/thumbnails',
-        '/home/*/domains/*/public_html/storage/framework/sessions',
+        '/home/*/domains/*/public_html/storage/framework/cache/*',
+        '/home/*/domains/*/public_html/storage/framework/views/*',
+        '/home/*/domains/*/public_html/userfiles/cache/*',
+        '/home/*/domains/*/public_html/userfiles/media/thumbnails/*',
+        '/home/*/domains/*/public_html/storage/framework/sessions/*',
 
     ];
 
