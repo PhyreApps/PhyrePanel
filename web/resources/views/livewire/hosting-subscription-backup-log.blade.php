25463d66b1c0feb6f6e288a2c356e498647bea9e
Bozhidar <bobi@microweber.com>
2024-05-16 15:22:55 +0300

    Update hosting-subscription-backup-log.blade.php

--- web/resources/views/livewire/hosting-subscription-backup-log.blade.php
+++ web/resources/views/livewire/hosting-subscription-backup-log.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <div id="js-backup-log" wire:poll="pullBackupLog" class="text-left text-sm font-medium text-gray-950 dark:text-yellow-500 h-[20rem] overflow-y-scroll">
+    <div id="js-backup-log" wire:poll.750ms="pullBackupLog" class="text-left text-sm font-medium text-gray-950 dark:text-yellow-500 h-[20rem] overflow-y-scroll">
 
         @if($this->backupLog == '')
             <div class="text-center text-gray-500 dark:text-gray-400">
