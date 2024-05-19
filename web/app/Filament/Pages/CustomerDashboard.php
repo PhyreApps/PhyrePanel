dca4d1cdcc4af5d27fa00f4ca61fed484d0c4c26
Daniela <69676022+bobicloudvision@users.noreply.github.com>
2024-05-20 00:18:09 +0300

    update

--- web/app/Filament/Pages/CustomerDashboard.php
+++ web/app/Filament/Pages/CustomerDashboard.php
@@ -21,6 +21,7 @@ class CustomerDashboard extends Page
     {
         return [
             'menu' => [
+
                 'email'=>[
                     'title'=>'Email',
                     'icon'=>'phyre_customer-php',
@@ -32,6 +33,86 @@ class CustomerDashboard extends Page
                         ],
                         [
                             'title'=>'Forwarders',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Email Routing',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Autoresponders',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Default Address',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Mailing Lists',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Track Delivery',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Global Email Filters',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Email Filters',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Email Deliverability',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Address Importer',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Spam Filters',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Encryption',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'BoxTrapper',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Calendars and Contacts Configuration',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Calendars and Contacts Sharing',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Calendars and Contacts Management',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Email Disk Usage',
                             'icon'=>'heroicon-o-star',
                             'link'=>'#'
                         ]
@@ -51,7 +132,129 @@ class CustomerDashboard extends Page
                             'title'=>'Manage Biling Information',
                             'icon'=>'heroicon-o-star',
                             'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Download Resources',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'View Email History',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'View Invoice History',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Search our Knowledgebase',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Check Network Status',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'View Billing Information',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Manage Profile',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Register New Domain',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Transfer a Domain',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Open Ticket',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'View Support Tickets',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Upgrade/Downgrade',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
                         ]
+                    ]
+                ],
+
+                'files'=>[
+                    'title'=>'Files',
+                    'icon'=>'heroicon-o-star',
+                    'menu'=>[
+                        [
+                            'title'=>'File Manager',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Images',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Directory Privacy',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Disk Usage',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Web Disk',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'FTP Accounts',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'FTP Connections',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Backup',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Backup Wizard',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'Git Version Control',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
+                        [
+                            'title'=>'File and Directory Restoration',
+                            'icon'=>'heroicon-o-star',
+                            'link'=>'#'
+                        ],
                     ]
                 ]
 
--- web/resources/views/filament/pages/customer-dashboard.blade.php
+++ web/resources/views/filament/pages/customer-dashboard.blade.php
@@ -1,5 +1,9 @@
 <div>
 
+
+    <meta http-equiv="refresh" content="60">
+
+
     <div>
         <h1>Tools</h1>
     </div>
@@ -7,22 +11,29 @@
     <div>
         @foreach($menu as $menuItem)
 
-            <div>
-                <div>
-                    <x-filament::icon-button size="xl" icon="{{$menuItem['icon']}}" />
-                    {{$menuItem['title']}}
+            <div class="bg-white/10 mt-[2rem] rounded px-2 shadow-xl">
+                <div class="flex justify-between py-4">
+                    <div class="flex gap-2 px-4">
+                        <div class=""><x-filament::icon-button size="xl" icon="{{$menuItem['icon']}}" /></div>
+                        <div class="">{{$menuItem['title']}}</div>
+                    </div>
+                    <div>
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                        </svg>
+                    </div>
                 </div>
 
-                <div>
+                <div class="border-t border-white/10 pt-2"></div>
+                <div class="grid lg:grid-cols-3 sm:grid-cols-2 grid-cols-1">
                     @foreach($menuItem['menu'] as $menuItemLink)
 
-                        <div>
-                            <a href="{{$menuItemLink['link']}}">
+                        <div class="text-blue-400 hover:text-yellow-500 px-2">
+                            <a href="{{$menuItemLink['link']}}" class="flex px-2 py-2">
                                 <x-filament::icon-button size="xl" icon="{{$menuItemLink['icon']}}" />
-                                {{$menuItemLink['title']}}
+                                <div class="ml-2">{{$menuItemLink['title']}}</div>
                             </a>
                         </div>
-
 
                     @endforeach
                 </div>
