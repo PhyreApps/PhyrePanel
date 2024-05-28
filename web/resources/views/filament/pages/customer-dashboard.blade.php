832d4df196abda14968181d9ade2320436e0abfa
Daniela <69676022+bobicloudvision@users.noreply.github.com>
2024-05-28 15:28:36 +0300

    Update customer-dashboard.blade.php

--- web/resources/views/filament/pages/customer-dashboard.blade.php
+++ web/resources/views/filament/pages/customer-dashboard.blade.php
@@ -28,7 +28,7 @@
                     <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-3">
                         <button type="button" class="-mr-1 flex p-2 rounded-md hover:bg-blue-500 sm:-mr-2">
                             <span class="sr-only">Dismiss</span>
-                            <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                            <svg class="h-6 w-6 text-white ml-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
                             </svg>
                         </button>
