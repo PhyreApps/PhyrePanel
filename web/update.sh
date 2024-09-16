126b4ae3bfefb701e73a04b49e3bb9f2cc5a13f5
Bozhidar <bobi@microweber.com>
2024-09-16 13:35:33 +0300

    Update update.sh

--- web/update.sh
+++ web/update.sh
@@ -2,6 +2,15 @@ PHYRE_PHP=/usr/local/phyre/php/bin/php
 
 
 
+printf "Updating the panel...\n"
+wget https://raw.githubusercontent.com/PhyreApps/PhyrePanelNGINX/main/compilators/debian/nginx/nginx.conf -O /usr/local/phyre/nginx/conf/nginx.conf
+
+mkdir -p /usr/local/phyre/ssl
+cp /usr/local/phyre/web/server/ssl/phyre.crt /usr/local/phyre/ssl/phyre.crt
+cp /usr/local/phyre/web/server/ssl/phyre.key /usr/local/phyre/ssl/phyre.key
+
+systemctl restart phyre
+systemctl status phyre
 
 printf "Updating the database...\n"
 $PHYRE_PHP artisan migrate
