f369197a2ec8c34b1a2118354d3f8fd6e6629c51
Bozhidar <bobi@microweber.com>
2024-09-19 14:01:40 +0300

    update

---
+++ web/app/PhyreBlade.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App;
+
+use Illuminate\Support\Facades\Blade;
+
+class PhyreBlade
+{
+    public static function render($file, $data = [])
+    {
+        $namespace = '';
+        if (strpos($file, '::') !== false) {
+            [$namespace, $file] = explode('::', $file);
+        }
+
+        $file = str_replace('.', '/', $file);
+        // Replace last / with .
+        $file = preg_replace('/\/([^\/]*)$/', '.$1', $file);
+
+        $hints = app()->view->getFinder()->getHints();
+        if (isset($hints[$namespace])) {
+            $path = $hints[$namespace][0] . '/' . $file;
+        }
+
+        if (!is_file($path)) {
+            throw new \Exception('File not found: ' . $path);
+        }
+
+        $content = file_get_contents($path);
+
+        $compiled = Blade::render($content, $data);
+
+        return $compiled;
+
+    }
+}
