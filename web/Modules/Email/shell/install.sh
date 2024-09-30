6ae9e2eb6227a6da87153a8543f1bc1c0bc4414f
Bozhidar <bobi@microweber.com>
2024-09-30 18:18:32 +0300

    Update install.sh

--- web/Modules/Email/shell/install.sh
+++ web/Modules/Email/shell/install.sh
@@ -9,6 +9,20 @@ sudo apt-get --no-install-recommends install clamav clamav-daemon -yq
 sudo apt-get --no-install-recommends install amavisd-new -yq
 sudo apt-get install libmysqlclient-dev libopendbx1-mysql -yq
 
+# Install SASL
+sudo apt-get install sasl2-bin -yq
+
+# auto start SASL file
+echo 'START=yes' > /etc/default/saslauthd
+
+systemctl restart saslauthd
+
+groupadd -g 5000 vmail && mkdir -p /var/mail/vmail
+useradd -u 5000 vmail -g vmail -s /usr/sbin/nologin -d /var/mail/vmail
+chown -R vmail:vmail /var/mail/vmail
+mkdir -p /etc/postfix/sql
+
+
 # Enable email ports
 ufw allow 25
 ufw allow 587
