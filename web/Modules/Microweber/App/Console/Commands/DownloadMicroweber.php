cee2359b14abc8cd91480df0745c110ff4442570
Bozhidar Slaveykov <bobi@microweber.com>
2024-12-11 20:32:52 +0200

    Create DownloadMicroweber.php

---
+++ web/Modules/Microweber/App/Console/Commands/DownloadMicroweber.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Modules\Microweber\App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class DownloadMicroweber extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'microweber:download';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $download = new \Modules\Microweber\App\Console\Commands\DownloadMicroweber();
+        $download->handle();
+    }
+}
