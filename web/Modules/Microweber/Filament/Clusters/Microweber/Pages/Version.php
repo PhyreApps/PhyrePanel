4000ad1af96b596bb6be9657928ebfe06dad4f4c
Bozhidar <bobi@microweber.com>
2025-01-17 17:22:06 +0200

    Update Version.php

--- web/Modules/Microweber/Filament/Clusters/Microweber/Pages/Version.php
+++ web/Modules/Microweber/Filament/Clusters/Microweber/Pages/Version.php
@@ -77,7 +77,7 @@ class Version extends Page
             }
         }
 
-        $debug = 1;
+        $debug = 0;
         if ($debug) {
             $dm = new DownloadMicroweber();
             $dm->handle();
