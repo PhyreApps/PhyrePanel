6f4a5e92b6fcd2f515a4d3ac1e9275eb42a8c48d
Bozhidar Slaveykov <bobicloudvision@gmail.com>
2025-01-29 17:19:06 +0200

    update

--- installers/ubuntu-22.04/install.sh
+++ installers/ubuntu-22.04/install.sh
@@ -97,6 +97,16 @@ fi
 # Go to web directory
 cd /usr/local/phyre/web
 
+
+mysql -uroot -proot <<MYSQL_SCRIPT
+SET GLOBAL validate_password.policy = LOW;
+SET GLOBAL validate_password.length = 6;
+SET GLOBAL validate_password.mixed_case_count = 0;
+SET GLOBAL validate_password.number_count = 0;
+SET GLOBAL validate_password.special_char_count = 0;
+FLUSH PRIVILEGES;
+MYSQL_SCRIPT
+
 # Create MySQL user
 MYSQL_PHYRE_ROOT_USERNAME="phyre"
 MYSQL_PHYRE_ROOT_PASSWORD="$(tr -dc a-za-z0-9 </dev/urandom | head -c 32; echo)"
