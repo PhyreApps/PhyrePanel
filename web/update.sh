6891b398c50c42fb16cddd903fd7d73c36622761
Bozhidar Slaveykov <bobi@microweber.com>
2025-02-17 17:45:16 +0200

    Update update.sh

--- web/update.sh
+++ web/update.sh
@@ -54,9 +54,9 @@ dpkg -i /usr/local/phyre/update/nginx/phyre-nginx-1.24.0-$OS_LOWER-$OS_VERSION.d
 printf "Updating the panel...\n"
 wget https://raw.githubusercontent.com/PhyreApps/PhyrePanelNGINX/main/compilators/debian/nginx/nginx.conf -O /usr/local/phyre/nginx/conf/nginx.conf
 #
-mkdir -p /usr/local/phyre/ssl
-cp /usr/local/phyre/web/server/ssl/phyre.crt /usr/local/phyre/ssl/phyre.crt
-cp /usr/local/phyre/web/server/ssl/phyre.key /usr/local/phyre/ssl/phyre.key
+# mkdir -p /usr/local/phyre/ssl
+# cp /usr/local/phyre/web/server/ssl/phyre.crt /usr/local/phyre/ssl/phyre.crt
+# cp /usr/local/phyre/web/server/ssl/phyre.key /usr/local/phyre/ssl/phyre.key
 
 systemctl restart phyre
 #systemctl status phyre
