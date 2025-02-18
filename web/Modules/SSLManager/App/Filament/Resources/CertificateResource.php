4a4f18f2f82a67e40f23e7e0a19511665212d009
Bozhidar Slaveykov <bobi@microweber.com>
2025-02-18 17:29:58 +0200

    Update CertificateResource.php

--- web/Modules/SSLManager/App/Filament/Resources/CertificateResource.php
+++ web/Modules/SSLManager/App/Filament/Resources/CertificateResource.php
@@ -71,12 +71,13 @@ class CertificateResource extends Resource
                 //
             ])
             ->actions([
-                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+//                Tables\Actions\EditAction::make(),
             ])
             ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
+//                Tables\Actions\BulkActionGroup::make([
+//                    Tables\Actions\DeleteBulkAction::make(),
+//                ]),
             ]);
     }
 
