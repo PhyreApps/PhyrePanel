1b684b01c49a9289fc7036cd380788d65413d6ab
Peter Ivanov <peter@microweber.com>
2025-05-13 14:40:44 +0300

    update

--- web/Modules/SSLManager/App/Jobs/SecureDomain.php
+++ web/Modules/SSLManager/App/Jobs/SecureDomain.php
@@ -29,11 +29,101 @@ class SecureDomain
         if (empty($domainName)) {
             throw new \Exception('Domain name is empty');
         }
+        $generalSettings = Settings::general();
+
         $domainNameWww = 'www.' . $domainName;
         $domainNameWww = str_replace('www.www.', 'www.', $domainNameWww);
 
+        $domainNameForWildcard = str_replace('www.', '', $domainName);
+        //get oly tld domain
 
-        $generalSettings = Settings::general();
+        $domainNameForWildcard = preg_replace('/^([^.]+\.)?([^.]+)\.([^.]+)$/', '$2.$3', $domainNameForWildcard);
+
+        $masterDomain = $generalSettings['master_domain'] ?? null;
+
+        //   $findWildcardSslAllValid = DomainSslCertificate::where('domain', '*.' . $domainNameForWildcard)->first();
+        $findWildcardSslAllValid = DomainSslCertificate::where('is_wildcard', 1)
+            //    ->where('domain', '*.' . $domainNameForWildcard)
+            ->where('is_active', 1)
+            ->where('is_auto_renew', 1)
+            ->get();
+
+        //   "domain" => "*.cloud.microweber.me"
+
+
+        if ($findWildcardSslAllValid) {
+            foreach ($findWildcardSslAllValid as $wildcardSsl) {
+
+                if (!$wildcardSsl->domain) {
+                    continue;
+                }
+
+                $domainCheck = str_replace('www.', '', $wildcardSsl->domain);
+                $domainCheck = str_replace('*.', '.', $domainCheck);
+
+                if (str_ends_with($domainNameForWildcard, $domainCheck)) {
+
+
+                    $sslCertificateFilePath = '/etc/ssl-manager/domains/' . $domainName . '/cert.pem';
+                    $sslCertificateKeyFilePath = '/etc/ssl-manager/domains/' . $domainName . '/privkey.pem';
+                    $sslCertificateChainFilePath = '/etc/ssl-manager/domains/' . $domainName . '/fullchain.pem';
+                    if (!is_dir('/etc/ssl-manager/domains/' . $domainName)) {
+                        shell_exec('mkdir -p /etc/ssl-manager/domains/' . $domainName);
+                    }
+
+                    file_put_contents($sslCertificateFilePath, $wildcardSsl->certificate);
+                    file_put_contents($sslCertificateKeyFilePath, $wildcardSsl->private_key);
+                    file_put_contents($sslCertificateChainFilePath, $wildcardSsl->certificate_chain);
+                    if (!file_exists($sslCertificateFilePath)
+                        || !file_exists($sslCertificateKeyFilePath)
+                        || !file_exists($sslCertificateChainFilePath)) {
+                        // Cant get all certificates
+                        throw new \Exception('Cant get wildcard certificates');
+                    }
+                    $sslCertificateFileContent = file_get_contents($sslCertificateFilePath);
+                    $sslCertificateKeyFileContent = file_get_contents($sslCertificateKeyFilePath);
+                    $sslCertificateChainFileContent = file_get_contents($sslCertificateChainFilePath);
+                    if (!empty($sslCertificateChainFileContent)) {
+                        $validateCertificates['certificate'] = $sslCertificateFileContent;
+                    }
+                    if (!empty($sslCertificateKeyFileContent)) {
+                        $validateCertificates['private_key'] = $sslCertificateKeyFileContent;
+                    }
+                    if (!empty($sslCertificateChainFileContent)) {
+                        $validateCertificates['certificate_chain'] = $sslCertificateChainFileContent;
+                    }
+                    if (count($validateCertificates) !== 3) {
+                        // Cant get all certificates
+                        throw new \Exception('Cant get all certificates');
+                    }
+
+                    DomainSslCertificate::where('domain', $findDomain->domain)->delete();
+
+
+                    $websiteSslCertificate = new DomainSslCertificate();
+                    $websiteSslCertificate->domain = $findDomain->domain;
+                    $websiteSslCertificate->certificate = $validateCertificates['certificate'];
+                    $websiteSslCertificate->private_key = $validateCertificates['private_key'];
+                    $websiteSslCertificate->certificate_chain = $validateCertificates['certificate_chain'];
+                    $websiteSslCertificate->customer_id = $findDomain->customer_id;
+                    $websiteSslCertificate->is_active = 1;
+                    $websiteSslCertificate->is_wildcard = 0;
+                    $websiteSslCertificate->is_auto_renew = 1;
+
+                    $websiteSslCertificate->provider = 'WILDCARD';
+                    $websiteSslCertificate->save();
+
+                    $findDomain->configureVirtualHost(true);
+
+                    return;
+                }
+
+                // $wildcardSsl->domain = str_replace('www.', '', $wildcardSsl->domain);
+            }
+        } else {
+            $findWildcardSslAllValid = null;
+        }
+
 
         $sslCertificateFilePath = '/etc/ssl-manager/domains/' . $domainName . '/cert.pem';
         $sslCertificateKeyFilePath = '/etc/ssl-manager/domains/' . $domainName . '/privkey.pem';
@@ -69,12 +159,12 @@ class SecureDomain
         $zerSslCertIntermediate = '/root/.acme.sh/' . $domainName . '_ecc/ca.cer';
         $zerSslCertFullChain = '/root/.acme.sh/' . $domainName . '_ecc/fullchain.cer';
 
-            if (!file_exists($zerSslCert)
-                || !file_exists($zerSslCertKey)
-                || !file_exists($zerSslCertFullChain)) {
-                // Cant get all certificates
-                throw new \Exception('Cant get certificates with ZeroSSL');
-            }
+        if (!file_exists($zerSslCert)
+            || !file_exists($zerSslCertKey)
+            || !file_exists($zerSslCertFullChain)) {
+            // Cant get all certificates
+            throw new \Exception('Cant get certificates with ZeroSSL');
+        }
 
 
         file_put_contents($sslCertificateFilePath, file_get_contents($zerSslCert));
